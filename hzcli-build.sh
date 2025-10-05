9c42b4edf5ff34429f9bfe84a097465e3c1f9910
univero <1449610641@qq.com>
2025-10-05 14:41:36 +0800

    fix: hz-client.yml

--- hzcli-build.sh
+++ hzcli-build.sh
@@ -1,5 +1,1 @@
-idl_dir=${IDL_DIR:=./idl}
-if [ $? != 0 ];then
-  exit 1
-fi
-hz client -I $idl_dir --idl=./api.thrift --model_dir=./hzcli/model --client_dir=./hzcli -t=template=slim
+hz client -I $idl_dir --idl=./idl/api.thrift --model_dir=./hzcli/model --client_dir=./hzcli -t=template=slim
