6aa145910c21f0b473af09b21eb35336070dbf19
univero <1449610641@qq.com>
2025-10-05 14:39:07 +0800

    fix: hz-client.yml

--- hzcli-build.sh
+++ hzcli-build.sh
@@ -1,5 +1,5 @@
 idl_dir=${IDL_DIR:=./idl}
-module_name=$(grep '^module' go.mod | awk '{print $2}')
 if [ $? != 0 ];then
   exit 1
-hz client --mod=$module_name -I $idl_dir --idl=./api.thrift --model_dir=./hzcli/model --client_dir=./hzcli -t=template=slim
+fi
+hz client -I $idl_dir --idl=./api.thrift --model_dir=./hzcli/model --client_dir=./hzcli -t=template=slim
