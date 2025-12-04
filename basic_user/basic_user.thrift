fc14752ff235dfa7573e4e9aac9e89aaf37d7ba6
Eagle233 <eagle233real@outlook.com>
2025-12-04 17:08:30 +0800

    feat: basicUser 登陆新增额外的 authType

--- basic_user/basic_user.thrift
+++ basic_user/basic_user.thrift
@@ -14,8 +14,9 @@ struct BasicUser {
 struct BasicUserRegisterReq {
     1:   string          authType,  // 认证类型
     2:   string          authId,    // 认证id
-    3:   string          verify,    // 认证凭证
-    4:   optional string password,  // 是否初始化密码
+    3:   optional string extraAuthId, // 扩展认证id
+    4:   string          verify,    // 认证凭证
+    5:   optional string password,  // 是否初始化密码
     255: base.App        app,
 }
 
@@ -29,7 +30,8 @@ struct BasicUserRegisterResp {
 struct BasicUserLoginReq {
     1: string authType,
     2: string authId,
-    3: string verify,
+    3: optional string extraAuthId, // 扩展认证id
+    4: string verify,
     255: base.App        app,
 }
 
